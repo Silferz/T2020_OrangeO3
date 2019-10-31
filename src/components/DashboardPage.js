faa79cc4ac0d95a0774d30a5ad1c3d9a954dedfc
Mark Ng <mark.ng.2015@sis.smu.edu.sg>
2019-10-31 15:53:18 +0800

    Transaction Graphs

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 
 class DashboardPage extends React.Component {
     constructor(props) {
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -19,27 +19,98 @@ class Transactions extends Component {
       visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
       data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
     }
+    
+// Money Out
+// Cash33.38%
+// Transfer16.92%
+// Uncategorized13.66%
+// Transportation10.27%
+// Shopping8.28%
+// Others
 
-    chart.data = data;
+    chart.data =  [ {
+      category: "Cash Back",
+      value: 8786,
+      open: 0,
+      stepValue: 8786,
+      color: chart.colors.getIndex( 17 ),
+      displayValue: "10% of S$8786",
+    }, {
+      category: "Expense",
+      value: 8786 - 2786,
+      open: 8786,
+      stepValue: 8786 - 2786,
+      color: chart.colors.getIndex( 8 ),
+      displayValue: 2786
+    }, {
+      category: "Investments",
+      value: 8786 - 2786 - 1786,
+      open: 8786 - 2786,
+      stepValue: 8786 - 2786 - 1786,
+      color: chart.colors.getIndex( 9 ),
+      displayValue: 1786
+    }, {
+      category: "Loans",
+      value: 8786 - 2786 - 1786 - 453,
+      open: 8786 - 2786 - 1786,
+      stepValue: 8786 - 2786 - 1786 - 453,
+      color: chart.colors.getIndex( 10 ),
+      displayValue: 453
+    }, {
+      category: "Credit Card",
+      value: 8786 - 2786 - 1786 - 453 - 1465,
+      open: 8786 - 2786 - 1786 - 453,
+      stepValue: 8786 - 2786 - 1786 - 453 - 1465,
+      color: chart.colors.getIndex( 16 ),
+      displayValue: 1465
+    }, {
+      category: "DBS Coin",
+      value: 8786 - 2786 - 1786 - 453 + 1465,
+      open: 0,
+      color: chart.colors.getIndex( 13 ),
+      displayValue: 8786 - 2786 - 1786 - 453 + 1465
+    } ];
+    
 
-    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "category";
+categoryAxis.renderer.minGridDistance = 40;
 
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.tooltip.disabled = true;
-    valueAxis.renderer.minWidth = 35;
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-    let series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.dateX = "date";
-    series.dataFields.valueY = "value";
+var columnSeries = chart.series.push(new am4charts.ColumnSeries());
+columnSeries.dataFields.categoryX = "category";
+columnSeries.dataFields.valueY = "value";
+columnSeries.dataFields.openValueY = "open";
+columnSeries.fillOpacity = 0.8;
+columnSeries.sequencedInterpolation = true;
+columnSeries.interpolationDuration = 1500;
 
-    series.tooltipText = "{valueY.value}";
-    chart.cursor = new am4charts.XYCursor();
+var columnTemplate = columnSeries.columns.template;
+columnTemplate.strokeOpacity = 0;
+columnTemplate.propertyFields.fill = "color";
 
-    let scrollbarX = new am4charts.XYChartScrollbar();
-    scrollbarX.series.push(series);
-    chart.scrollbarX = scrollbarX;
+var label = columnTemplate.createChild(am4core.Label);
+label.text = "{displayValue.formatNumber('$#,## a')}";
+label.align = "center";
+label.valign = "middle";
 
+
+var stepSeries = chart.series.push(new am4charts.StepLineSeries());
+stepSeries.dataFields.categoryX = "category";
+stepSeries.dataFields.valueY = "stepValue";
+stepSeries.noRisers = true;
+stepSeries.stroke = new am4core.InterfaceColorSet().getFor("alternativeBackground");
+stepSeries.strokeDasharray = "3,3";
+stepSeries.interpolationDuration = 2000;
+stepSeries.sequencedInterpolation = true;
+
+// because column width is 80%, we modify start/end locations so that step would start with column and end with next column
+stepSeries.startLocation = 0.1;
+stepSeries.endLocation = 1.1;
+
+chart.cursor = new am4charts.XYCursor();
+chart.cursor.behavior = "none";
     this.chart = chart;
   }
 
